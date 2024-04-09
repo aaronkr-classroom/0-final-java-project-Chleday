297bb2315195744664d0a5d698c73581f6ca6109
Chleday <162276728+Chleday@users.noreply.github.com>
2024-04-09 16:58:46 +0900

    6주차 과제끝

--- Welcome.java
+++ Welcome.java
@@ -6,11 +6,12 @@ public class Welcome {
 	static final int NUM_ITEM = 7;
 	static CartItem[] mCartItem = new CartItem[NUM_BOOK];
 	static int mCartCount = 0;
+	static User mUser;
 	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
 		String[][] mBook = new String[NUM_BOOK][NUM_ITEM];
+		
+		Scanner input = new Scanner(System.in);
 		
 		System.out.print("당신의 이름을 입력하세요: ");
 		String name = input.next();
@@ -18,6 +19,7 @@ public class Welcome {
 		System.out.print("연락처를 입력하세요: ");
 		int phone = input.nextInt(); //숫자만 입력
 		
+		mUser = new User(name, phone);
 		String greeting = "Welcome to Shopping Mall!";
 		String tagline = "Welcome to book Market!!";
 		
@@ -42,10 +44,14 @@ public class Welcome {
 			
 			System.out.println("메뉴 번호를 선택하세요: ");
 			int choice = input.nextInt();
-			
+			/*
 			if (choice < 1 || choice > 8) {
 				System.out.println("1부터 8까지의 숫자를 입력하세요.");
-			} else {
+			} 
+			*/
+			if (choice < 1 || choice > 9) {
+				System.out.println("1부터 9까지의 숫자를 입력하세요.");
+			}	else {
 				switch(choice) {
 				case 1:
 					
@@ -76,6 +82,9 @@ public class Welcome {
 					menuCartExit();
 					quit = true;
 					break;
+				case 9:
+					menuAdminLogin();
+					break;
 				} //Switch 끝
 			} //if-else 끝
 		} // while 끝
@@ -92,6 +101,7 @@ public class Welcome {
 		System.out.println("2. 장바구니 상품 목록보기 \t6. 장바구니에 항목 수량 줄이기");
 		System.out.println("3. 장바구니 비우기 \t7. 장바구니에 항목 삭제하기");
 		System.out.println("4. 영수증 표시하기 \t8. 종료");
+		System.out.println("9. 관리자 로그인");
 		System.out.println("****************************");
 	}
 	/**
@@ -103,7 +113,8 @@ public class Welcome {
 	 */
 	public static void menuGuestInfo(String name, int phone) {
 		System.out.println("1. 현재 고객 정보: ");
-		System.out.println("이름 " + name + " 연락처: " + phone);
+		
+		System.out.println("이름 " + mUser.getName() + " 연락처: " + mUser.getPhone());
 	}
 	/**
 	 * 설명: 2번 상품목록보기
@@ -165,6 +176,7 @@ public class Welcome {
 						mCartItem[mCartCount++] = new CartItem(book[numID]);
 				}
 				quit = true;
+				input.close();
 			} else
 				System.out.println("다시 입력해 주세요");
 		}
@@ -215,6 +227,23 @@ public class Welcome {
 		book[2][4] = "컴퓨팅 사고력을 키우는 블록 코딩";
 		book[2][5] = "컴퓨터입문";
 		book[2][6] = "2019/06/10";
+	}
+	public static void menuAdminLogin() {
+		System.out.println("관리자 정보를 입력하세요.");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("아이디: ");
+		String adminID = input.next();
+		
+		System.out.print("비밀번호: ");
+		String adminPW = input.next();
+		
+		Admin admin = new Admin(mUser.getName(), mUser.getPhone());
+		if (adminID.equals(admin.getId()) && adminPW.equals(admin.getPassword())) {
+			System.out.println("이름 " + admin.getName() + " 연락처" + admin.getPhone());
+			System.out.println("아이디 " + admin.getId() + " 비밀번호" + admin.getPassword());
+		} else
+			System.out.println("관리자 정보가 일치하지 않습니다.");
 	}
 } //welcome 클래스 끝
 
